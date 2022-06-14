29cfc17bc4567d293da77ef19ad99f079b99a51b
akhil anand sm <akhil.anand@ilearningengines.com>
2022-06-14 12:40:02 +0530

    React router domV6 donot use switch anymore and Movies section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/detail' element={<Detail/>} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import styled from 'styled-components'
 import ImageSlider from './ImageSlider'
 import Viewers from './Viewers'
+import Movies from './Movies'
 
 function Home() {
   return (
     <Container>
       <ImageSlider />
       <Viewers />
+      <Movies />
     </Container>
   )
 }
